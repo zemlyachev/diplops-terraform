a49ef35cebf520495da3df9089b44eeddef588b9
Andrey Zemlyachev <kvintilian@gmail.com>
2024-07-14 21:43:18 +0300

    NAT Gateway для воркеров без внешнего адреса

--- vpc.tf
+++ vpc.tf
@@ -10,4 +10,22 @@ resource "yandex_vpc_subnet" "subnet" {
   zone           = "ru-central1-${each.key}"
   network_id     = yandex_vpc_network.network-diplom.id
   v4_cidr_blocks = [each.value]
+  route_table_id = yandex_vpc_route_table.rt.id
+}
+
+resource "yandex_vpc_gateway" "nat_gateway" {
+  folder_id = var.YC_FOLDER_ID
+  name      = "test-gateway"
+  shared_egress_gateway {}
+}
+
+resource "yandex_vpc_route_table" "rt" {
+  folder_id  = var.YC_FOLDER_ID
+  name       = "test-route-table"
+  network_id = yandex_vpc_network.network-diplom.id
+
+  static_route {
+    destination_prefix = "0.0.0.0/0"
+    gateway_id         = yandex_vpc_gateway.nat_gateway.id
+  }
 }
